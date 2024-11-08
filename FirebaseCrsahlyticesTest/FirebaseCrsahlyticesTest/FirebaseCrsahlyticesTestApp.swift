4b03cc3b34fd41f9b7b57cd1008783db8746e15b
shafiqullah <shohagbit0404@gmail.com>
2024-11-08 15:36:29 -0800

    Fresh demo project created
    Signed-off-by: shafiqullah <shohagbit0404@gmail.com>

---
+++ FirebaseCrsahlyticesTest/FirebaseCrsahlyticesTest/FirebaseCrsahlyticesTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseCrsahlyticesTestApp.swift
+//  FirebaseCrsahlyticesTest
+//
+//  Created by Shafiq  Ullah on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseCrsahlyticesTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
